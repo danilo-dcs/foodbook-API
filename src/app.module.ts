0a4e9c849f470691f6f16368a723dc62d83676d2
Danilo <danilopktcastro@gmail.com>
2021-07-12 12:50:22 -0300

    API 1.0 version

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,24 +9,13 @@ import { FoodModule } from './food/food.module';
 
     ConfigModule.forRoot({isGlobal: true}),
 
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DATABASE_HOST,
-    //   port: parseInt(<string>process.env.DATABASE_PORT),
-    //   database: process.env.DATABASE_NAME,
-    //   username: process.env.DATABASE_USERNAME,
-    //   password: process.env.DATABASE_PASSWORD,
-    //   autoLoadEntities: true,
-    //   synchronize: true
-    // }),
-
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: "ec2-3-231-69-204.compute-1.amazonaws.com",
+      host: process.env.DATABASE_HOST || process.env.DATABASE_LOCAL_HOST,
       port: 5432,
-      database: "d3nem2ave0gnsa",
-      username: "phedxikbxfvbtx",
-      password: "11d2e52a74f7c0be4f9d6e329df2679b5f41a0a01a43fb5ebd67c7db043b8ada",
+      database: process.env.DATABASE || process.env.DATABASE_LOCAL,
+      username: process.env.DATABASE_USER || process.env.DATABASE_LOCAL_USER  ,
+      password: process.env.DATABASE_PASSWORD || process.env.DATABASE_LOCAL_PASSWORD,
       autoLoadEntities: true,
       synchronize: true
     }),
