f9a810998040a7bda95a4526b70fd7cfdb6fd5f8
Danilo <danilopktcastro@gmail.com>
2021-07-14 18:36:04 -0300

    Elephant sql

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  // app.enableCors();
   await app.listen( process.env.PORT || 3000);
 }
 bootstrap();
